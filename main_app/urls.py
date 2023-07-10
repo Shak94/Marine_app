761dc97bfc3c50737e857b385efe6dc0969033d7
Shak94 <shakoi.1994@gmail.com>
2023-07-09 21:56:43 -0400

    Saved progress

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 from .models import Coral
+from .models import CoralTraits
 
 urlpatterns =[
     path('', views.Home.as_view(), name='home'),
@@ -9,5 +10,7 @@ urlpatterns =[
     path('coral/new/', views.CoralCreate.as_view(), name ="coral_create"),
     path('coral/<int:pk>', views.CoralDetail.as_view(), name ="coral_detail"),
     path('coral/<int:pk>/update/',views.CoralUpdate.as_view(), name ="coral_update"),
-    path('coral/<int:pk>/delete/',views.CoralDelete.as_view(), name ="coral_delete")
+    path('coral/<int:pk>/delete/',views.CoralDelete.as_view(), name ="coral_delete"),
+    path('coral/<int:pk>/trait/new/', views.CoralTraitsCreate.as_view(), name="trait_create")
+
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -2,12 +2,13 @@ from typing import Any, Dict
 from django.shortcuts import render
 from django.views import View
 from django.views.generic.base import TemplateView
-from .models import Coral
+from .models import Coral , CoralTraits
 from django.views.generic.edit import CreateView
 from django.views.generic import DetailView
 from django.views.generic.edit import CreateView, UpdateView
 from django.urls import reverse
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.shortcuts import redirect
 # Create your views here.
 class Home(TemplateView):
     template_name ="home.html"
@@ -46,3 +47,13 @@ class CoralDelete(DeleteView):
      template_name = "coral_delete.html"
      def get_success_url(self):
         return reverse('coral_list')
+     
+class CoralTraitsCreate(View):
+
+    def post(self, request, pk):
+        diet= request.POST.get("diet")
+        toxicity = request.POST.get("toxicity")
+        rareity = request.POST.get("rareity")
+        coral = Coral.objects.get(pk=pk)
+        CoralTraits.objects.create(diet=diet, toxicity=toxicity, rareity=rareity)
+        return redirect('coral_detail', pk=pk)
